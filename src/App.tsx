cc5592105154e943dfb9c0445a5e77f9471b5e80
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-02-25 15:56:39 +0530

    setting up basic project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quisquam ratione in facere, dignissimos harum tenetur repudiandae incidunt dolore aspernatur atque asperiores, illum consectetur aliquid consequuntur minima obcaecati eos eum!
+    </>
+  )
+}
+
+export default App
